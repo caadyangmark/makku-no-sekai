1ddd85f3010fb02b5081cd64e201de70f5a858b3
Mark Caadyang <marknoecaadyang@gmail.com>
2024-02-02 22:14:37 +0800

    initial components

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Makku no Sekai</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog Entry</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
