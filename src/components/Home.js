5a3dc1daa4277b34e095fb158ee80aeef2144426
Mark Caadyang <marknoecaadyang@gmail.com>
2024-02-05 19:48:57 +0800

    added props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 
 const Home = () => {
@@ -26,12 +27,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title={"All Blogs"}></BlogList>
             {/* <h2>Homepage</h2>
             <h1>{ name }</h1>
             <button onClick={handleClickReactive}>Click me!</button>
